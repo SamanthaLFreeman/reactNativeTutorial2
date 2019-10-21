fe7a8c8022bf7f5438cdb73b0b7f6ce18bb4c860
Alan Turing <freemasl.sf@gmail.com>
2019-10-20 19:43:05 -0600

    Make the colors and box shadow modular

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import Card from '../components/Card';
+import Colors from '../constants/colors';
 
 const StartGameScreen = () => {
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start a New Game!</Text>
-      <View style={styles.inputContainer}>
+        <Card style={styles.inputContainer}>
         <Text>Select a Number</Text>
         <TextInput />
         <View style={styles.buttonContainer}>
-          <Button title="Reset"  />
-          <Button title="Confirm" />
+          <View style={styles.button}>
+            <Button title='Reset' color={Colors.accent} />
+          </View>
+          <View style={styles.button}>
+            <Button title='Confirm' color={Colors.primary} />
+          </View>
         </View>
-      </View>
+      </Card>
     </View>
   )
 }
@@ -30,21 +36,16 @@ const styles = StyleSheet.create({
   inputContainer: {
     width: 300,
     maxWidth: '80%',
-    alignItems: 'center',
-    shadowColor: 'black',
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 6,
-    shadowOpacity: 0.26,
-    elevation: 6,
-    backgroundColor: 'white',
-    padding: 20,
-    borderRadius: 10
+    alignItems: 'center'
   },
   buttonContainer: {
     flexDirection: 'row',
     width: '100%',
     justifyContent: 'space-between',
     paddingHorizontal: 15
+  },
+  button: {
+    width: 100
   }
 })
 
